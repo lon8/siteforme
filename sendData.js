ca68719d536ca47290eb74cb5b7962f62f2a437e
lon8 <vlad.kanatik@ya.ru>
2023-03-28 23:44:40 +0200

    lol

---
+++ sendData.js
@@ -0,0 +1,8 @@
+function sendData(){
+    const data = {name = document.getElementById('name'), email: document.getElementById('email'), message=  document.getElementById('message')};
+    console.log(data);
+    // fetch() = {
+
+    // }
+
+}
